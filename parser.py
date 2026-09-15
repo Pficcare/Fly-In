46862d0c8e4f047c3b91c9e128233283bac79d57
Pficcare <pficcare@student.42lisboa.com>
2026-09-15 19:36:00 +0100

    Start to add dataclass Zone instead of dictonary to store zone/node datas

--- parser.py
+++ parser.py
@@ -1,78 +1,93 @@
 #!/usr/bin/python3
-import sys
+# import sys
+from dataclasses import dataclass
+
+HUB_KEYS = frozenset(["nb_drones", "start_hub", "end_hub", "hub", "connection"])
+
+@dataclass(frozen=True)
+class Zone:
+    name: str
+    coordo: tuple[int,int]
+    zone_status: str = "normal"
+    color:str | None = None
+    max_drones:int = 1
 
 class PrinceOfParser:
-    def __init__(self, instruction:str):
-        self.vertex:dict = {}
+    def __init__(self, instruction: str):
+        self.vertex: dict = {}
         self.instruction = instruction
         self.map_drone_lmt = 0
+        self.start: Zone| None
+        self.end: Zone | None
 
     def split_not_spit(self):
-        #definir dees gabarits pour stocker les valeurs:
-        self.start_hub:dict = {"coordo": (0,0), "color": None}
-        self.hub:dict = {"coordo": (0,0), "zone": "normal", "color": None, "max_drones": 1}
-        self.end_hub:dict = {"coordo": (0,0), "color": None}
-        self.list_param: list = []
-        
         for line in self.instruction.strip().splitlines():
-            if not line.strip(): # si que " " return false
+            line, _, _ = line.partition("#")
+
+            if not line.strip():  # si que " " 
                 continue
             hub, _, datas = line.partition(":")
             hub = hub.strip()
+
+            if hub not in HUB_KEYS:
+                raise KeyError
+
             if hub == "nb_drones":
-                nb = int(datas)
+                nb = self.to_int(datas)
                 self.map_drone_lmt = nb
-
-            if hub == "start_hub" or hub == "end_hub":
-                id_node = self.start_hub.copy() # sans cpy,on passe l adresse a id donc le meme dict
-                id_node["voisin"] = {}
-                if hub == "end_hub":
-                    id_node = self.end_hub.copy()
-                    id_node["voisin"] = {}
+                
+            elif hub == "start_hub" or hub == "end_hub":
                 datas, _, params = datas.partition("[")
-                node, x,y = datas.split()
-                (x,y) = self.convertion(x,y) 
-                id_node["coordo"] = (x,y)
-                p1, p2 = params.split("=")
+                node, x, y = datas.split()
+                (x, y) = self.convertion(x, y)
+                if len(params) != 2:
+                    raise ValueError
+                p1, p2 = params.split("=") #color=green
+                color: str | None = None
                 p2 = p2.strip("]")
-                if p1 in id_node:
-                    id_node[p1] = p2
+                if p1 == "color":
+                    color = p2
+                elif p1 != "max_drones":
+                    raise ValueError
+                z = Zone(node,(x,y),"normal", color)
+                if hub == "start_hub":
+                    self.start = z
                 else:
-                    print(f"Error {p1} is not a valid data")
-                self.vertex[node] = id_node
+                    self.end = z
+                self.vertex[node] = z
 
-            if hub == "hub":
+            elif hub == "hub":
                 datas, _, params = datas.partition("[")
-                node, x,y = datas.split()
-                (x,y) = self.convertion(x,y) 
+                node, x, y = datas.split()
+                (x, y) = self.convertion(x, y)
                 id_node = self.hub.copy()
                 id_node["voisin"] = {}
-                id_node["coordo"] = (x,y)
+                id_node["coordo"] = (x, y)
                 self.list_param = params.split()
                 for i in range(len(self.list_param)):
                     data = self.list_param[i]
-                    p1,p2 = data.split("=")
+                    p1, p2 = data.split("=")
                     p2 = p2.strip("]")
                     if p1 in id_node and p1 == "max_drones":
                         id_node[p1] = int(p2)
                     elif p1 in id_node:
                         id_node[p1] = p2
                     else:
-                        raise ValueError 
+                        raise ValueError
                         print(f"{p1} is not valid")
                 self.vertex[node] = id_node
 
-            if hub == "connection":
-                datas,_, params = datas.partition("[")
+            elif hub == "connection":
+                datas, _, params = datas.partition("[")
                 datas = datas.strip()
                 if not params:
-                    node,nbor = datas.split("-")
-                    node = node.strip() 
+                    node, nbor = datas.split("-")
+                    node = node.strip()
                     nbor = nbor.strip()
                     nb = 1
                 else:
-                    node,nbor = datas.split("-")
-                    node = node.strip() 
+                    node, nbor = datas.split("-")
+                    node = node.strip()
                     nbor = nbor.strip()
                     link, nb = params.split("=")
                     link = link.strip()
@@ -83,19 +98,125 @@ class PrinceOfParser:
                 self.vertex[node]["voisin"][nbor] = nb
                 self.vertex[nbor]["voisin"][node] = nb
 
+        print(self.vertex.items())
 
+         
 
-        print (self.vertex.items())
-
-    def convertion(self, x: str, y:str) -> tuple[int,int]:
+    @staticmethod
+    def convertion(x: str, y: str) -> tuple[int, int]:
         try:
             a = int(x)
             b = int(y)
-        except ValueError as e:
-            raise e
-        return (a,b)
+        except ValueError:
+            raise ValueError
+        return (a, b)
+
+    @staticmethod
+    def to_int(value: str) -> int:
+        try:
+            nb = int(value)
+        except ValueError:
+            raise ValueError
+        if nb < 1:
+            raise ValueError
+        else:
+            return nb
 
 
+
+# class PrinceOfParser:
+#     def __init__(self, instruction: str):
+#         self.vertex: dict = {}
+#         self.instruction = instruction
+#         self.map_drone_lmt = 0
+#
+#     def split_not_spit(self):
+#         # definir dees gabarits pour stocker les valeurs:
+#         self.start_hub: dict = {"coordo": (0, 0), "color": None}
+#         self.hub: dict = {
+#             "coordo": (0, 0),
+#             "zone": "normal",
+#             "color": None,
+#             "max_drones": 1,
+#         }
+#         self.end_hub: dict = {"coordo": (0, 0), "color": None}
+#         self.list_param: list = []
+#
+#         for line in self.instruction.strip().splitlines():
+#             line, _, _ = line.partition("#")
+#             if not line.strip():  # si que " " return false
+#                 continue
+#             hub, _, datas = line.partition(":")
+#             hub = hub.strip()
+#             if hub not in HUB_KEYS:
+#                 raise KeyError
+#             if hub == "nb_drones":
+#                 nb = int(datas)
+#                 self.map_drone_lmt = nb
+#
+#             elif hub == "start_hub" or hub == "end_hub":
+#                 id_node = (
+#                     self.start_hub.copy()
+#                 )  # sans cpy,on passe l adresse a id donc le meme dict
+#                 id_node["voisin"] = {}
+#                 if hub == "end_hub":
+#                     id_node = self.end_hub.copy()
+#                     id_node["voisin"] = {}
+#                 datas, _, params = datas.partition("[")
+#                 node, x, y = datas.split()
+#                 (x, y) = self.convertion(x, y)
+#                 id_node["coordo"] = (x, y)
+#                 p1, p2 = params.split("=")
+#                 p2 = p2.strip("]")
+#                 if p1 in id_node:
+#                     id_node[p1] = p2
+#                 else:
+#                     print(f"Error {p1} is not a valid data")
+#                 self.vertex[node] = id_node
+#
+#             elif hub == "hub":
+#                 datas, _, params = datas.partition("[")
+#                 node, x, y = datas.split()
+#                 (x, y) = self.convertion(x, y)
+#                 id_node = self.hub.copy()
+#                 id_node["voisin"] = {}
+#                 id_node["coordo"] = (x, y)
+#                 self.list_param = params.split()
+#                 for i in range(len(self.list_param)):
+#                     data = self.list_param[i]
+#                     p1, p2 = data.split("=")
+#                     p2 = p2.strip("]")
+#                     if p1 in id_node and p1 == "max_drones":
+#                         id_node[p1] = int(p2)
+#                     elif p1 in id_node:
+#                         id_node[p1] = p2
+#                     else:
+#                         raise ValueError
+#                         print(f"{p1} is not valid")
+#                 self.vertex[node] = id_node
+#
+#             elif hub == "connection":
+#                 datas, _, params = datas.partition("[")
+#                 datas = datas.strip()
+#                 if not params:
+#                     node, nbor = datas.split("-")
+#                     node = node.strip()
+#                     nbor = nbor.strip()
+#                     nb = 1
+#                 else:
+#                     node, nbor = datas.split("-")
+#                     node = node.strip()
+#                     nbor = nbor.strip()
+#                     link, nb = params.split("=")
+#                     link = link.strip()
+#                     nb = nb.strip("]")
+#                     nb = int(nb)
+#                 if node not in self.vertex or nbor not in self.vertex:
+#                     raise KeyError
+#                 self.vertex[node]["voisin"][nbor] = nb
+#                 self.vertex[nbor]["voisin"][node] = nb
+#
+#         print(self.vertex.items())
 
 
     # def split_not_spit(self)-> dict | None:
@@ -180,7 +301,7 @@ class PrinceOfParser:
 #         if start == "nb_drones":
 #             drones_data = int(txt)
 #         elif "hub" in start:
-#             node_info, _, param = txt.partition("[")            
+#             node_info, _, param = txt.partition("[")
 #             node,x,y = node_info.split()
 #             param.rsplit("]")
 #             colorinfo, max_drones = param.split()
@@ -201,11 +322,11 @@ class PrinceOfParser:
 #     print(vertex)
 
 
-
 # Exemple:gate1 1 0[color=orange max_drones=1]
 
+
 def main() -> None:
-    
+
     try:
         with open("./maps/easy/02_simple_fork.txt", "r", encoding="utf-8") as file:
             instruction = file.read()
@@ -216,8 +337,6 @@ def main() -> None:
 
     p = PrinceOfParser(instruction)
     parsed = p.split_not_spit()
-
-
 
 
 if __name__ == "__main__":
